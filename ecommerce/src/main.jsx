c86a325782da724feb63fdcc9aeb9456aaf37c03
wbx <2062214930@qq.com>
2026-03-12 13:51:32 +0800

    Create multiple pages using react-router

--- ecommerce/src/main.jsx
+++ ecommerce/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// ✅ 修正：从 react-router-dom 导入
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
